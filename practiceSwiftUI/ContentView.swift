316fcef2e73e9c648b040026ab6ca6a16fa25d50
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2023-12-02 17:30:16 +0900

    Initial Commit

---
+++ practiceSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  practiceSwiftUI
+//
+//  Created by 천광조 on 12/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practiceSwiftUI/practiceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceSwiftUIApp.swift
+//  practiceSwiftUI
+//
+//  Created by 천광조 on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct practiceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
