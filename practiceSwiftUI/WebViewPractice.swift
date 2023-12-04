5e553ba56c962987aedd113f1a3345c764987b0f
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2023-12-04 22:16:31 +0900

    WebView 공부 (SwiftUI 에서 UIkit의 UIView 사용해보기)

---
+++ practiceSwiftUI/WebViewPractice.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewPractice.swift
+//  practiceSwiftUI
+//
+//  Created by 천광조 on 12/4/23.
+//
+
+import SwiftUI
+
+struct WebViewPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebViewPractice()
+}
