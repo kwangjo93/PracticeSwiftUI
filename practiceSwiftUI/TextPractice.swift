c7a55e11ad3138eef683c52e4fe77ce621be474c
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2023-12-05 21:50:55 +0900

    Text

---
+++ practiceSwiftUI/TextPractice.swift
@@ -0,0 +1,18 @@
+//
+//  TextPractice.swift
+//  practiceSwiftUI
+//
+//  Created by 천광조 on 12/5/23.
+//
+
+import SwiftUI
+
+struct TextPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextPractice()
+}
