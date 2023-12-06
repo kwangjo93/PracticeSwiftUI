a1be5925a3e9ea3a7b7fc536fd8211095ef2b9e2
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2023-12-06 18:30:43 +0900

    Image Practice

---
+++ practiceSwiftUI/ImagePractice.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePractice.swift
+//  practiceSwiftUI
+//
+//  Created by 천광조 on 12/6/23.
+//
+
+import SwiftUI
+
+struct ImagePractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePractice()
+}
